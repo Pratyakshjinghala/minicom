1856912289cecdfbf444316d8cb222b9222b1fde
Pratyakshjinghala <cpratyaksh864@gmail.com>
2025-09-18 10:27:58 +0530

    fix error

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ import categoriesRoute from "./routes/categories.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://minicom.vercel.app"
+}));
 app.use(express.json());
 
 // Routes
